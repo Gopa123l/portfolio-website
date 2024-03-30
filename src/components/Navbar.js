101af70995cfdde6e7f6439e6d74ab0a076fe7d3
Gopa123l <f20200278@pilani.bits-pilani.ac.in>
2024-03-30 14:33:50 +0530

    Add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = () => {
             </Link>
             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                 <li>
-                    <Link to='/'>Home</Link>
+                    <Link to='/portfolio-website'>Home</Link>
                 </li>
                 <li>
                     <Link to='/project'>Projects</Link>
